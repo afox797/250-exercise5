3c89a0136bbc33f74c1ffacbf28c0c6db25d6a0c
afox <axof3873@outlook.com>
2022-04-09 11:42:17 -0400

    Documentation for regex

--- findconn.py
+++ findconn.py
@@ -3,7 +3,21 @@ import re
 
 
 def search(path: str) -> list:
-    regex = re.compile(r'mysql://((?!\.\.).)*?/[A-Za-z]([^\s]+)')
+    # Create re object using the specified regex below.
+
+    regex = re.compile(r'mysql://'           # Beginning of connection string will always start with mysql://
+                       
+                       r'((?!\.\.).)*?/'     # Use a negative look ahead (?!) to exclude double periods but accept any 
+                                             # other characters before the forward slash. Use a lazy match (*?) to 
+                                             # allow for the possibility of multiple matches per line. If a greedy match
+                                             # is used, the regex will match the very last forward slash of the line.
+                                             # This will match both the server name and port, if the port is included.
+                       
+                       r'[A-Za-z]([^\s]+)')  # The database name must start with a letter [A-Za-z] and can contain
+                                             # letters, digits, and underscores. This regex matches anything that comes
+                                             # after the server name or port starting with a letter until the first
+                                             # whitespace ([^\s]+).
+
     connections_list = []
     file = open(path, 'r')
 
